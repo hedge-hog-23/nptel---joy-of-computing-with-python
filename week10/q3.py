aae64414d7f82376b53dd3bc116734b0ea7680fd
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2023-10-04 19:55:29 +0530

    Create q3.py

---
+++ week10/q3.py
@@ -0,0 +1,12 @@
+# Given a string s write a program to convert uppercase letters into lowercase and lowercase letters into uppercase. Also print the resultant string.
+
+# Note: You need to talk the input and do not print anything while taking input.
+
+# Input:
+# The Joy Of Computing
+
+# Output
+# tHE jOY oF cOMPUTING
+
+s = input()
+print(s.swapcase())
